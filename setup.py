b86f935bcd0d1e3cdf40d0d26c59bac9f9600745
Abhay Nigam <36666689+mashuk999@users.noreply.github.com>
2022-06-16 23:33:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="kstopit",
-    version="0.0.10",
+    version="0.0.11",
     author="Kristof",
     description="kstopit",
     long_description=long_description,
