27ba845a368257cefb5b60df86243ac255240375
kkristof200 <kovacskristof200@gmail.com>
2023-05-30 13:54:08 +0300

    Update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="kstopit",
-    version="0.0.15",
+    version="0.0.16",
     author="Kristof",
     description="kstopit",
     long_description=long_description,
